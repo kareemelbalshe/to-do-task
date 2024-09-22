cc8a600cd3ba2f20a39a134b9fbaab49b331ae17
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2024-09-22 14:11:08 +0300

    'kareem'

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: "https://to-do-task-26qr.onrender.com",
+    baseURL: "https://to-do-task-1.onrender.com",
 })
 //https://blog-app-mern-fprg.onrender.com
 
