b6d6b97ef65754fb7dcd1e547ebef4afb2c1ad06
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2024-11-24 18:56:40 +0200

    fix error

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import img from "../../images/task-list.png"
 import { Link } from "react-router-dom";
 import "./home.css";
 
 export default function Home() {
   return (
     <div className="home">
-        <div className="img"><img src={img} alt="img" /></div>
+        <div className="img"><img src={"/assets/images/task-list.png"} alt="img" /></div>
       <div className="content">
         <h1>Welcome to My To-Do app</h1>
         <p>It's a great way to manage your tasks and stay organized.</p>
